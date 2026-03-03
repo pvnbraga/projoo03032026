8022fbfdd7c3b723e3a7b3f8e0f0a8da2457200e
pvnbraga <pvnbraga1@uniara.edu.br>
2026-03-03 19:35:17 -0300

    v0

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+    try {
+
+        System.out.println("Digite um numero: ");
+        int a = sc.nextInt();
+
+        System.out.println("Digite outro numero: ");
+        int b = sc.nextInt();
+
+        int resultado = a / b;
+        System.out.println("O resultado é: " + resultado);
+
+    } catch (ArithmeticException e){
+
+        System.out.println("Erro, não da para dividir por 0.");
+    }catch (InputMismatchException ex){
+        System.out.println("Erro: você precisa inserir apenas numeros " + ex.getMessage());
+    }catch (Exception ex2){
+        System.out.println("Erro: " + ex2.getMessage());
+    }
+    }
+}
